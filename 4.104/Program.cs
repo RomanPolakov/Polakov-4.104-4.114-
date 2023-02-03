2e7adfa3252a4e21ba1adbd3f0afdce0470a03da
RomanPolakov <113889059+RomanPolakov@users.noreply.github.com>
2023-02-03 09:40:05 +0300

    Add files via upload

---
+++ 4.104/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите номер недели");
+int a = int.Parse(Console.ReadLine());
+switch(a)
+{
+    case 1:
+        Console.WriteLine(day.Monday);
+        break;
+    case 2:
+        Console.WriteLine(day.Tuesday);
+        break;
+    case 3:
+        Console.WriteLine(day.Wednesday);
+        break;
+    case 4:
+        Console.WriteLine(day.Thursday);
+        break;
+    case 5:
+        Console.WriteLine(day.Friday);
+        break;
+    case 6:
+        Console.WriteLine(day.Saturday);
+        break;
+    case 7:
+        Console.WriteLine(day.Sunday);
+        break;
+    default:
+        Console.WriteLine("Такого дня нет");
+        break;
+
+}
+Console.ReadKey();
+enum day
+{
+    Monday,
+Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+
+}
